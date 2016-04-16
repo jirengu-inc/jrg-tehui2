bcc2c9aca81dee008ec4dd7998dc4dc0bb4d0068
wmsj100 <wmsj100@hotmail.com>
2016-04-17 00:36:59 +0800

    0416

--- homework/王浩/task27/task-27-4_get.php
+++ homework/王浩/task27/task-27-4_get.php
@@ -2,13 +2,25 @@
 /**
  * 
  * @authors Your Name (you@example.org)
- * @date    2016-04-15 16:49:23
+ * @date    2016-04-15 16:59:12
  * @version $Id$
  */
-
-$content=array(
-	"img05"=>"img/05.jpg",
-	"img06"=>"img/06.jpg",
-	"img07"=>"img/07.jpg",
-	"img08"=>"img/08.jpg",
-	)
+$status = $_REQUEST["status"];
+// $status = "1";
+if($status === "1"){
+	$content = array(
+		"img05" => "img/05.jpg",
+		"img06" => "img/06.jpg",
+		"img07" => "img/07.jpg",
+		"img08" => "img/08.jpg",
+		"img01" => "img/01.jpg",
+		"img02" => "img/02.jpg",
+		"img03" => "img/03.jpg",
+		"img04" => "img/04.jpg"
+	);
+	$num =array("num" => count($content));
+	$status = array("status" => "success");
+	$wrap = array($status,$content,$num);
+	echo json_encode($wrap);
+	// echo count($content);
+}
