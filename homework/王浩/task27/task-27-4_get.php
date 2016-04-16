7e0c7ceb695678babdf4c89158f91ee2ce05ce47
wmsj100 <wmsj100@hotmail.com>
2016-04-17 00:22:52 +0800

    task 27

---
+++ homework/王浩/task27/task-27-4_get.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-04-15 16:49:23
+ * @version $Id$
+ */
+
+$content=array(
+	"img05"=>"img/05.jpg",
+	"img06"=>"img/06.jpg",
+	"img07"=>"img/07.jpg",
+	"img08"=>"img/08.jpg",
+	)
