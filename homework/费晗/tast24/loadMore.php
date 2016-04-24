96aafd580625898bbd783e73ec9da47c2bc78a6d
feihan <@964959033@qq.com>
2016-04-24 20:01:09 +0800

    tast-24

--- homework/费晗/tast24/loadMore.php
+++ homework/费晗/tast24/loadMore.php
@@ -4,7 +4,7 @@
     $len = $_GET["len"];
     $items = array();
     for ($i = 0; $i < $len; $i++) {
-        array_push($items, "内容". ($start + $i));
+        array_push($items, "内容". ($start + $i+1));
     }
     $ret = array("status"=>1, "data"=>$items);
     sleep(2);  //测试响应未完成时的状态
