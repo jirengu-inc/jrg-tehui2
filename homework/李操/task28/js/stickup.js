40400e44a0ce0e86278e48ce1a74dc66ab9cabef
GardonLee <15720600373@163.com>
2016-04-25 12:16:53 +0800

    add

--- homework/李操/task28/js/stickup.js
+++ homework/李操/task28/js/stickup.js
@@ -1,55 +1,55 @@
 //为jQuery对象扩展一个stickup插件
 
 (function ($) {
-$.fn.stickup = function (handler) {
-	 var $cur = this,
-	 	 curHeight = $cur.height(),
-	 	 curWidth = $cur.width(),
-	 	 offsetTop = $cur.offset().top,
-	 	 offsetLeft = $cur.offset().left,
-	 	 onChange = handler || function () {};
+	$.fn.stickup = function (handler) {
+		 var $cur = this,
+		 	 curHeight = $cur.height(),
+		 	 curWidth = $cur.width(),
+		 	 offsetTop = $cur.offset().top,
+		 	 offsetLeft = $cur.offset().left,
+		 	 onChange = handler || function () {};
 
-	//克隆一个并让他隐藏
-	var $clone = $cur.clone()//链式写法
-				.css('opacity',0)//保证看到的只有一个
-				.insertBefore($cur)
-				.hide();
+		//克隆一个并让他隐藏
+		var $clone = $cur.clone()//链式写法
+					.css('opacity',0)//保证看到的只有一个
+					.insertBefore($cur)
+					.hide();
 
-	$(window).on('scroll',function () {
-		 if ($(this).scrollTop() >= offsetTop) {
-		 	if (!isFixed()) {
-		 		// console.log(1);
-		 		setFixed();
-		 	}
-		 }else{
-		 	if(isFixed()){
-		 		unsetFixed();
-		 	}
-		 }
-	});
+		$(window).on('scroll',function () {
+			 if ($(this).scrollTop() >= offsetTop) {
+			 	if (!isFixed()) {
+			 		// console.log(1);
+			 		setFixed();
+			 	}
+			 }else{
+			 	if(isFixed()){
+			 		unsetFixed();
+			 	}
+			 }
+		});
 
-	function isFixed () {
-		 return !!$cur.attr('fixed');
-	}
+		function isFixed () {
+			 return !!$cur.attr('fixed');
+		}
 
-	function setFixed () {
-		 $cur.attr('fixed',true)//绑定自定义属性作为标识位
-		 .css({
-		 	position: 'fixed',
-		 	top: 0,
-		 	left: offsetLeft,
-		 	'z-index': 999,
-		 	width: curWidth,
-		 	margin: 0
-		 });
-		 $clone.show();
-	}
+		function setFixed () {
+			 $cur.attr('fixed',true)//绑定自定义属性作为标识位
+			 .css({
+			 	position: 'fixed',
+			 	top: 0,
+			 	left: offsetLeft,
+			 	'z-index': 999,
+			 	width: curWidth,
+			 	margin: 0
+			 });
+			 $clone.show();
+		}
 
-	function unsetFixed () {
-		 $cur.removeAttr('fixed') 
-		 .removeAttr('style');
-		 $clone.hide();
-	}
-};
+		function unsetFixed () {
+			 $cur.removeAttr('fixed')
+			 .removeAttr('style');
+			 $clone.hide();
+		}
+	};
 
 })(jQuery)
