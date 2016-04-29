713df60f72004d2140a74e4ff31f63ae57af6916
Tamamoran <liuyejunnan@163.com>
2016-04-29 17:19:44 +0800

    fix bug

--- homework/刘叶/task29/task29-3/getMore.php
+++ homework/刘叶/task29/task29-3/getMore.php
@@ -1,14 +1,13 @@
 <?php
     // 后端 php 测试接口文件
-    $start = $_GET['start']; //2
-    $len = $_GET['len'];  //6
+    $start = $_GET['start']; //10
+    $len = $_GET['len'];  //5
     $items = array();
 
     for($i = 0; $i < $len; $i++){
         array_push($items, '内容' . ($start+$i));
     }
     $ret = array('status'=>1, 'data'=>$items);
-
+    sleep(1);//测试loading图用
     //{status: 1, data: ['内容1','内容2','内容3']}
-    sleep(1.0);//测试用，让loading更明显
     echo json_encode($ret);
