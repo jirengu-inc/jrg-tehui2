7ef54f237b7b9d30766c4f7c452f8578dbc00224
peifan325 <526648262@qq.com>
2016-09-03 17:46:09 +0800

    weaterfall

---
+++ homework/尹配环/Weaterfall/js/weaterfall.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Administrator on 2016/5/15.
+ */
+/**
+ * 瀑布流
+ * $ct 将要设置的元素集合的父元素
+ * listArr : 列高度数组
+ * @type {{init: WeaterFuil.init, setCss: WeaterFuil.setCss, findMin: WeaterFuil.findMin}}
+ */
+
+
+var Weaterfall = {
+    init:function ($nodes,listArr) {
+        this.$nodes = $nodes;
+        this.nodeW = this.$nodes.outerWidth(true);
+        this.listArr = listArr;
+
+        this.initArr();
+        this.setCss();
+    },
+    initArr:function () {
+        var me = this;
+        var len = me.listArr.length;
+
+        if (len == 0) {            //初次使用列高度数组，全部初始化为0
+            var colNum = parseInt(me.$nodes.parent().width() / me.$nodes.outerWidth(true));
+            for (var i=0; i<colNum ; i++){
+                me.listArr.push(0);
+            }
+        }
+    },
+    setCss:function () {
+        var me =this;
+
+        me.$nodes.each(function (index, el) {
+            var $cur = $(this);
+
+            me.findMin();
+            $cur.css({
+                left: me.nodeW * me.idx,
+                top:me.min
+            });
+            me.listArr[me.idx] += $cur.outerHeight(true);
+
+        });
+        console.log("瀑布流完成");
+    },
+    findMin:function () {
+        var me = this;
+        me.min = me.listArr[0];
+        me.idx = 0;
+
+        for (var key in me.listArr){
+            if (me.listArr[key] < me.min) {
+                me.min = me.listArr[key];
+                me.idx = key;
+            }
+        }
+    }
+};
